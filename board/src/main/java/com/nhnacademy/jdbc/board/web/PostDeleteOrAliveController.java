6eab5d20938cf172b74daf5a141530075caebd05
유호철 <hochul-y@naver.com>
2022-05-20 20:00:22 +0900

    Add : PostDeleteOrAliveController 로 수정

---
+++ board/src/main/java/com/nhnacademy/jdbc/board/web/PostDeleteOrAliveController.java
@@ -0,0 +1,25 @@
+package com.nhnacademy.jdbc.board.web;
+
+import com.nhnacademy.jdbc.board.post.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PostDeleteOrAliveController {
+    private final PostService postService;
+
+    public PostDeleteOrAliveController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping(value = {"/delete","/alive"})
+    public ModelAndView delete(@RequestParam("postNo") Long postNo){
+        ModelAndView mav = new ModelAndView("redirect:showPosts");
+        postService.removePostOrViewPost(postNo);
+
+        return mav;
+    }
+
+}
