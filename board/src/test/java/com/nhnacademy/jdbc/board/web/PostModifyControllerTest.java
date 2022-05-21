d8c646c34dee96c8bb9d8e02fe7ea8b8f93b1864
Hochul <hochul-y@naver.com>
2022-05-21 12:27:37 +0900

    ADD testcode 작성중 ...

---
+++ board/src/test/java/com/nhnacademy/jdbc/board/web/PostModifyControllerTest.java
@@ -0,0 +1,22 @@
+package com.nhnacademy.jdbc.board.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostModifyControllerTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void modify() {
+    }
+
+    @Test
+    void doModify() {
+    }
+}
